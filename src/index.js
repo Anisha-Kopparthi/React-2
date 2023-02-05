9fd743264e11a1159276871cbb61f28dc9c4a837
Anisha-Kopparthi <21b01a1283@svecw.edu.in>
2023-02-05 21:31:33 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import reactdom from 'react-dom/client';
+import App from './app'
+
+const el = document.getElementById('root')
+const root = reactdom.createRoot(el)
+
+root.render(
+    <>
+        <App/>
+    </>
+)
